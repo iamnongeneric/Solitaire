5eeb2d1aa92e93a52b763f0033ace9af1722dc46
Vyacheslav Palamar <vyacheslav.palamar@gmail.com>
2016-03-29 22:03:54 +0300

    Give whole solitaire as parameter instead of just cards

--- js/deckContainer.js
+++ js/deckContainer.js
@@ -1,7 +1,7 @@
 var DeckContainer = (function () {
   function DeckContainer(options) {
     this._el = options.element;
-    this._cards = options.deckCards;
+    this._solitaire = options.solitaire;
     this._template = document.getElementById('deck_template').innerHTML;
 
     this.render();
@@ -9,10 +9,13 @@ var DeckContainer = (function () {
 
   DeckContainer.prototype.render = function () {
     this._el.innerHTML = _.template(this._template)({
-      deckCards: this._cards
+      deckCards: this._solitaire.deck
     });
   }
 
+  DeckContainer.prototype.getElement = function () {
+    return this._el;
+  }
 
   return DeckContainer;
 })();
